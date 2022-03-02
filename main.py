7d8045231d29f75a554226f46c81cebc94af526b
Abir Zaher <99669228+AbirZaher97@users.noreply.github.com>
2022-03-03 01:40:39 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,53 @@
+# ---------------------------------------------------------------------------- #
+# Title: Assignment 07
+# Description: Working with pickling and handling errors try and except blocks
+# ChangeLog (Who,When,What):
+# RRoot,1.1.2030,Created started script
+# <Abir Zaher>,<1/03/2022>
+# ---------------------------------------------------------------------------- #
+
+# imports code form another code file
+import pickle
+
+# Data -------------------------------------------- #
+strFileName = 'AppData.dat'
+customerID = ""
+lstCustomer = []
+customerName = ""
+
+# Processing -------------------------------------- #
+
+# create try and except block for invalid input
+try:
+    print("Please enter your information below")
+    print()  # print for good looks
+    customerID = int(input("Please enter your ID number: "))
+    customerName = str(input("Please enter your name: "))
+    lstCustomer=[customerID, customerName]
+    print(lstCustomer)
+except Exception as e:
+    print("Please enter integer numbers only for your ID")
+    print(e, e.__doc__, type(e), sep="\n")
+
+# create try and except block to save data with pickling
+try:
+    file = open("AppData.dat", "ab")
+    # create pickle.dump to store data
+    pickle.dump(lstCustomer, file)
+    file.close()
+except Exception as e:
+    print("an error has occurred in file")
+    print(e, e.__doc__, type(e), sep="\n")
+
+try:
+    file = open("AppData.dat", "rb")
+    # create pickle.load to load original data
+    fileData = pickle.load(file)
+    file.close()
+    print(fileData)
+except FileNotFoundError as e:
+    print("File does not exist!")
+    print(e, e.__doc__, type(e), sep="\n")
+except Exception as e:
+    print("There was a general error")
+    print(e, e.__doc__, type(e), sep="\n")
